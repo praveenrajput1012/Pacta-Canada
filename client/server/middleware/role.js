a2b87c282bac1a9fa145419f238dda97e1ace2ca
Praveen Rajput <praveen-benzatine>
2025-08-03 18:51:52 +0530

    Updated axios baseURL for live backend

--- client/server/middleware/role.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(requiredRole) {
-    return (req, res, next) => {
-      if (req.user.role !== requiredRole) {
-        return res.status(403).json({ message: 'Forbidden: Insufficient permissions' });
-      }
-      next();
-    };
-  };
--- client/server/routes/search.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const searchCtrl = require("../controllers/searchController");
-
-router.get("/", searchCtrl.search);
-
-module.exports = router;
