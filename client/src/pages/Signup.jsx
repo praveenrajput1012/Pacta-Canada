6e9c3a0dc7f9f24a315171183cf2518d4c903984
Praveen Rajput <praveen-benzatine>
2025-08-03 19:34:46 +0530

    fix: Signup URL

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -19,7 +19,7 @@ const Signup = () => {
     e.preventDefault();
     setMessage("");
     try {
-      const res = await fetch("http://localhost:5000/api/auth/signup", {
+      const res = await fetch("https://pacta-canada-2.onrender.com/api/auth/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
